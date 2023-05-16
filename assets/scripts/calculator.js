5213fb51b76ca3c56167fe9ad95f9a7167b31c8b
marko <marko@marko.com>
2023-05-16 19:08:42 +0200

    2nd commit

---
+++ assets/scripts/calculator.js
@@ -0,0 +1,23 @@
+
+
+
+function add(){
+ console.log('added');
+}
+
+function subtract(){
+    console.log('subtracted');
+}
+
+function multiply(){
+    console.log('multiplied');
+}
+
+function divide(){
+    console.log('divided');
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
